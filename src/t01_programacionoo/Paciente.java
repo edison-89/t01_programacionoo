6fbc009fa6c4188f3be0882f5df10675cb01aaac
ediso <ediso@LAPTOP-S0K7O57C>
2021-06-22 20:00:30 -0500

    Cambios paras imprimir

--- src/t01_programacionoo/Paciente.java
+++ src/t01_programacionoo/Paciente.java
@@ -1,5 +1,13 @@
 package t01_programacionoo;
 
 public class Paciente {
+	private String nombre;
 
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
 }
